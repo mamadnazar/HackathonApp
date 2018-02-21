c210d3363eee430a53a6170367b5cfa80dc29daf
Emir <amanbekoff@gmail.com>
2018-02-21 19:04:30 +0600

    fixed some errors

---
+++ Hackathon/MainPage/BackTableView/BackTableVC.swift
@@ -0,0 +1,47 @@
+//
+//  BackTableVC.swift
+//  Hackathon
+//
+//  Created by ITLabAdmin on 2/21/18.
+//  Copyright © 2018 neobis. All rights reserved.
+//
+
+import UIKit
+
+class BackTableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableArray = ["Расписание","О хакатоне" , "Новости и объявления" , "Лайфхаки" , "Спонсоры" ,
+                              "Менторы" , "Вопросы и ответы" , "Правила поведения и участия" , "Списки команд" , "О приложении"]
+    
+    private let vcs = ["ScheduleVC" , "AboutHackathonVC" , "NewsVC" , "LifehacksVC" , "PartnersVC" , "MentorsVC" , "QuestionsVC" , "RulesVC" , "TeamsViewController" , "AboutAppVC"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = tableArray[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let revealVC = revealViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: vcs[indexPath.row])
+        revealVC?.pushFrontViewController(vc, animated: true)
+        
+    }
+}
+
